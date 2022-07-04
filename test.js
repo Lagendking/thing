3e9d14e15d3829e6b89d598b88912d85313a1a71
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-07-04 01:30:37 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,16 +2,7 @@ let redBox = document.getElementById("red")
 let greenBox = document.getElementById("green")
 let blueBox = document.getElementById("blue")
 
-redBox.onclick = () => {
-  console.log("red")
-}
-
-greenBox.onclick = () => {
-  console.log("green")
-}
-
-blueBox.onclick = () => {
-  console.log("blue")
-}
-
 const Buttons = document.querySelectorAll(".Clicky")
+Buttons.forEach(clicker => {
+  clicker.onclick => console.log(clicker.value)
+})
