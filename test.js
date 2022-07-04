092cbaa84d901e96ae2933c27797b0cb6b95763f
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-07-04 01:12:01 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
 let redBox = document.getElementById("red")
 let greenBox = document.getElementById("green")
 let blueBox = document.getElementById("blue")
+
+redBox.onclick = () => {
+  console.log("red")
+}
+
+greenBox.onclick = () => {
+  console.log("green")
+}
+
+blueBox.onclick = () => {
+  console.log("blue")
+}
