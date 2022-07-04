ce29a0d4770d5cda16c9df6aad4d4d67d5795c7b
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-07-04 01:35:15 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ let redBox = document.getElementById("red")
 let greenBox = document.getElementById("green")
 let blueBox = document.getElementById("blue")
 
+const clickAmount = {
+  "red": 0,
+  "green": 0,
+  "blue": 0,
+}
 const Buttons = document.querySelectorAll(".Clicky")
 Buttons.forEach(clicker => {
   clicker.onclick = () => console.log(clicker.value)
