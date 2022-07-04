4d37eb24307a7ca0ac8ab091f9539872613e1f5a
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-07-04 02:38:24 -0400

    Update test.js

--- test.js
+++ test.js
@@ -13,6 +13,10 @@ Buttons.forEach(clicker => {
   }
 })
 
+const clrbutton = document.getElementById("clear")
+
+clrbutton.onclick = () => clearScore()
+  
 const clearScore = () => {
   Buttons.forEach(clicker => clickAmount[clicker.value] = 0
 }
