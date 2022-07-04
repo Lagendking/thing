8f088e1b8a9788dc989973ec320b714f16a91d1d
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-07-04 16:10:33 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const clicker = document.getElementById("clicker")
 const amount = document.getElementById("amount")
 const upgrade = document.getElementById("upgrade")
+const upgrade1 = document.getElementById("upgrade1")
 
 const game = {
   "clicks": 0,
   "perClick": 1,
+  "perSecond": 0,
 }
 
 clicker.onclick = () => {
@@ -13,13 +15,32 @@ clicker.onclick = () => {
 }
 
 upgradeCost = Number(upgrade.value).toFixed(2)
+upgrade1Cost = Number(upgrade1.value).toFixed(2)
+
 upgrade.onclick = () => {
   if (game.clicks >= upgradeCost) {
     game.clicks -= upgradeCost
     game.perClick += 1
     upgradeCost *= 1.25
-    upgrade.innerText = `Upgrade $${upgradeCost.toFixed(2)}`
+    upgrade.innerText = `click +1 | ${upgradeCost.toFixed(2)}`
     amount.innerText = `${game.clicks.toFixed(2)} Clicks`
+  }
+}
+
+upgrade1.onclick = () => {
+  if (game.clicks >= upgrade1Cost) {
+    game.clicks -= upgrade1Cost
+    game.perSecond += 0.1
+    upgrade1Cost *= 1.25
+    upgrade1.innerText = `cps +0.1 | ${upgrade1Cost.toFixed(2)}`
+    amount.innerText = `${game.clicks.toFixed(2)} Clicks`
+  }
+}
+
+const givecps = () => {
+  while (1<2) {
+    await setTimeout(1)
+    game.clicks += game.perSecond
   }
 }
 
