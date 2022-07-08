b29a290283e9dd5b75abe00ee957daf35ea49adf
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-07-08 01:47:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const upgrade2 = document.getElementById("upgrade2")
 const upgrade3 = document.getElementById("upgrade3")
 const clicks = document.getElementById("clicks")
 const cps = document.getElementById("cps")
+const pupgrade1 = document.getElementById("pupg1")
+const info = document.getElementById("inf1")
+
+pupgrade1.addEventListener('mouseover', () => info.style.display = "initial")
+pupgrade1.addEventListener('mouseleave', () => info.style.display = "none")
 
 const game = {
     perClick: 1,
     perSecond: 0,
     clickAmount: 0,
+    cmulti: 1,
 }
 const bought = {
     up1: 0,
@@ -19,6 +25,7 @@ const bought = {
 let cost1 = Number(upgrade1.value)
 let cost2 = Number(upgrade2.value)
 let cost3 = Number(upgrade3.value)
+let pcost1 = Number(pupgrade1.value)
 
 const format = (num, nums) => {
     let suf = ""
@@ -34,20 +41,26 @@ const format = (num, nums) => {
     if (num >= 1e21) {newnum = num / 1e21, suf = "sx"}
     if (num >= 1e24) {newnum = num / 1e24, suf = "sp"}
     if (num >= 1e27) {newnum = num / 1e27, suf = "oc"}
-    suf = newnum.toFixed(nums) + suf
+    suf = newnum.toFixed(2) + suf
     return suf
 }
 
 const update = () => {
+    if (bought.up2 >= 5) {
+        upgrade3.style.display = "initial"
+    }
+    if (bought.up1 >= 10) {
+        pupgrade1.style.display = "initial"
+    }
     clicks.innerText = `${format(game.clickAmount, 0)} Clicks`
-    cps.innerText = `${format(game.perSecond, 2)} Cps`
+    cps.innerText = `${format(game.perSecond, 1)} Cps`
     upgrade1.innerText = `+1 click | $${format(cost1, 0)} | ${format(bought.up1, 0)}`
     upgrade2.innerText = `+0.5 Cps | $${format(cost2, 0)} | ${format(bought.up2, 0)}`
     upgrade3.innerText = `+2.5 Cps | $${format(cost3, 0)} | ${format(bought.up3, 0)}`
 }
 
 mainButton.onclick = () => {
-    game.clickAmount += game.perClick
+    game.clickAmount += game.perClick * game.cmulti
     update()
 }
 
@@ -81,10 +94,21 @@ upgrade3.onclick = () => {
     }
 }
 
-const givecps = setInterval(function(){
-    if (bought.up2 >= 5) {
-        upgrade3.style.display = "initial"
+const del = (obj) => {
+    obj.style.display="hidden"
+}
+
+pupgrade1.onclick = () => {
+    if (game.clickAmount >= pcost1) {
+        game.cmulti += 0.25
+        game.clickAmount -= pcost1
+        del(pupgrade1)
+        update()
     }
+}
+
+const givecps = setInterval(function(){
     game.clickAmount += game.perSecond / 10
+    pupgrade1.style.display = "none"
     update()
 }, 100)
